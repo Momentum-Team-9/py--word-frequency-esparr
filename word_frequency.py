a1e1be030c7f4f3e3f76ee909cb1dae94ca063fa
Em Sparr <amberem@gmail.com>
2021-08-04 19:09:23 -0400

    Word list is filtered, sorted, counted and added to a dictionary -- (-.-)Zzz...

--- word_frequency.py
+++ word_frequency.py
@@ -1,28 +1,40 @@
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
-]
+    'will', 'with', ''
+    ]
 
 def print_word_freq(file):
     """Read in `file` and print out the frequency of words in that file."""
     import string
+
     word_count = {}
+
     with open(file) as file_doc:
         text = file_doc.read()
-        for i in text:
+
+    text = text.replace("\n", " ")
+
+    for i in text:
             if i in string.punctuation:
                 text = text.replace(i, "")
-    text = text.replace("\n", " ")
-    text = text.lower()
-    text = text.split(" ")
     
-    print(type(text))
-    print(text)
-    print(file_doc.closed)
+    text = text.lower()
+    word_list = text.split(" ")
 
-# - remove punctuation
-# - normalize all words to lowercase
+    stop_word_set = set(STOP_WORDS)
+    filtered_word_list = [item for item in word_list if item not in stop_word_set]
+    sorted_word_list = sorted(filtered_word_list)
+
+    if i not in word_count:
+        for i in sorted_word_list:
+            word_count[i] = sorted_word_list.count(i)
+    print(word_count)
+
+
+    # print(type(text))
+    # print(file_doc.closed)
+
 # - remove "stop words" -- words used so frequently they are ignored
 # - go through the file word by word and keep a count of how often each word is used
 # - create a dictionary with value of word and a key of count. 
